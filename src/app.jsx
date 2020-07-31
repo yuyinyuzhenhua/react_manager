af768c25369907fb58bd53b732ad14ce562523d3
yuzhenhua <yuzhenhua@bjhike.com>
2020-07-31 15:16:35 +0800

    'init'

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+import './index.css'
+import './index.scss'
+import 'font-awesome/css/font-awesome.min.css'
+
+ReactDOM.render(
+    <h1><i className="fa fa-camera-retro fa-lg"></i>hello</h1>,
+    document.getElementById('root')
+)
